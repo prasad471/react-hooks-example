3e6b1233018d4a6a0e04e08b05e0c28dbac6db97
Bobbili <801269@cognizant.com>
2020-06-15 11:52:03 +0530

    refs example

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,12 @@ import React, { Component } from 'react';
 import PostList from './components/PostList';
 import NewPost from './components/NewPost';
 import Practice from './components/Practice';
+import demoUseEffect from './components/demoUseEffect';
+import useRefsTutorial from "./components/useRefsTutorial";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-// const stylesApp = {
-//   marginTop: 40
-// }
 
 class App extends Component {
   render() {
@@ -38,6 +37,16 @@ class App extends Component {
               Practice
             </Link>
             </li>
+            <li className="nav-link">
+            <Link to={'/useEffect'} className="nav-link">
+              useEffect
+            </Link>
+            </li>
+            <li className="nav-link">
+            <Link to={'/useRefs'} className="nav-link">
+              useRefs
+            </Link>
+            </li>
           </ul>
           </nav>
           <Switch>
@@ -45,6 +54,8 @@ class App extends Component {
             <Route exact path='/addPost'  render={() => <NewPost key="1" />} />
             <Route exact path='/practice' component = {Practice}/>
             <Route exact path='/editPost/:id' render={() => <NewPost key="2" />}/>
+            <Route exact path='/useEffect' component = {demoUseEffect}/>
+            <Route exact path='/useRefs' component = {useRefsTutorial}/>
           </Switch>
           
         </div>
